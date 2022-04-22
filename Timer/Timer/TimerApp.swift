2dcdf2946472f14a2de27df4d22ae8086ee12356
Baimurat Zhandos <76952603+Zhandos-Hello-World@users.noreply.github.com>
2022-04-22 16:15:08 +0600

    Created app

---
+++ Timer/Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Zhandos Baimurat on 22.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
